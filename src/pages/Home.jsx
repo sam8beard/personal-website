f6d77d278adcb91ff80152036c12da1fd862a848
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-03-01 02:21:36 -0500

    Starting homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import './Home.css'
 const Home = () => { 
     return (
-        <div>
-            <img src='/images/sammy.jpg' alt='A picture of Sammy'/>
+        <div class='parent-container'>
+            <div class='intro-container'> 
+                <img class = 'sammy-pic' src='/images/sammy.jpg' alt='A picture of Sammy'/>
+                <h1> Sammy Beard </h1>
+                <h2> B.S. Computer Science </h2>
+                <h3> hello </h3> 
+            </div>
+
+            <div class='testing'> 
+                <h1> Hello </h1>
+            </div>
         </div>
+
+        
     )
 }
 export default Home 
