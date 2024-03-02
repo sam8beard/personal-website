f9a283c0b10debe3c0820fdfb7b851ab82f579f6
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-03-02 15:43:21 -0500

    Changed root color and playing around with flex containers on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
 import './Home.css'
 const Home = () => { 
     return (
+        
         <div class='parent-container'>
-            <div class='intro-container'> 
+            <div class='parent-item parent-item-1'> 
                 <img class = 'sammy-pic' src='/images/sammy.jpg' alt='A picture of Sammy'/>
-                <h1> Sammy Beard </h1>
-                <h2> B.S. Computer Science </h2>
-                <h3> hello </h3> 
+                <h1> item 2</h1>
+                {/* <h1> Sammy Beard </h1> */}
+                {/* <p> 
+                    Hey! My name is Sammy. I am a Computer Science 
+                    student at the University of Georgia.
+                </p> */}
+            </div>
+            <div class='parent-item parent-item-2'> 
+                <h1> item 2 </h1>
             </div>
 
-            <div class='testing'> 
-                <h1> Hello </h1>
-            </div>
+
+         
         </div>
 
         
